8e9f2f6f60b0f6d61f5ac97d4f67aa54dd6abd3e
puenteprod06-bit <puenteprod06@gmail.com>
2026-03-08 18:57:58 -0300

    db.js

--- db.js
+++ db.js
@@ -1,22 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: "srv798.hstgr.io",
-  user: "u445028788_lamentemaestra",
-  password: ">Yd7/yo1?Vs",
-  database: "u445028788_lamentemaestra",
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10
-});
-
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("Error conectando a MySQL:", err);
-  } else {
-    console.log("Conectado a MySQL");
-    connection.release();
-  }
+  host: "localhost",
+  user: "usuario",
+  password: "password",
+  database: "database"
 });
 
 module.exports = db;
