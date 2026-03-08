b6867b3d94581d649aa9468b1ce75dbbcb0b91c3
puenteprod06-bit <puenteprod06@gmail.com>
2026-03-08 20:50:52 -0300

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = mysql.createPool({
   host: "localhost",
   user: "u445028788_lamentemaestra",
   password: "2N28slK!.!ZQ64[T",
-  database: "u445028788_lamentemaestra_db",
+  database: "u445028788_lamentemaestra",
   port: 3306
 });
 
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
+
 
 
 
