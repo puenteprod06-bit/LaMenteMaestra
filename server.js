30a638041347934f87ea7491d9418067204ee5ac
puenteprod06-bit <puenteprod06@gmail.com>
2026-03-08 18:39:52 -0300

    server.js

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 const express = require("express");
-const mysql = require("mysql2");
 
 const app = express();
 
-const db = mysql.createPool({
-  host: "srv798.hstgr.io",
-  user: "u445028788_lamentemaestra",
-  password: ">Yd7/yo1?Vs",
-  database: "u445028788_lamentemaestra",
-  port: 3306
-});
-
 app.get("/", (req, res) => {
-  db.query("SELECT 1", (err, result) => {
-    if (err) {
-      console.error(err);
-      res.send("Error conectando a MySQL");
-    } else {
-      res.send("Servidor y base de datos funcionando 🚀");
-    }
-  });
+  res.send("Servidor Node funcionando 🚀");
 });
 
 const PORT = process.env.PORT || 3000;
@@ -27,4 +11,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
-
