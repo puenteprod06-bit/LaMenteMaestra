877d6210e798aea3f3e16f6b29c067a7212f5cc4
puenteprod06-bit <puenteprod06@gmail.com>
2026-03-09 01:13:52 -0300

    server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2");
 const app = express();
 
 const db = mysql.createPool({
-  host: "auth-db798.hstgr.io",
+  host: "localhost",
   user: "u445028788_lamentemaestra",
   password: "0b?X#AW6y*F",
   database: "u445028788_lamentemaestra",
@@ -27,6 +27,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
 });
+
 
 
 
